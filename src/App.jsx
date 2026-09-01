493f8748a3d44aec5273321a94898b48187d44aa
ROSHINI-076 <roshirose2405@gmail.com>
2026-09-01 22:57:07 +0530

    initial deloyment

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return(
+    <div>
+      <h1>Welcome to kaar tech practice</h1>
+      <p>My first automated deployment  </p>
+    </div>
+  );
+}
+
+export default App
